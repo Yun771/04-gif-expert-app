73e5c96735dbf46ba5232be592cbb6d8a5fe4507
emartinez <ever.martinez@grupotum.com>
2023-01-24 12:24:02 -0600

    first commit

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,9 +1,39 @@
+import { useState } from "react";
+
 export const GifExpertApp = () => {
+
+  const [categories, setCategories ] = useState([ 'One Punch', 'Dragon Ball' ]);
+
+  const onAddCategory = () => {
+  
+      // categories.push('Hola')
+      // setCategories([...categories, 'Hola mundo']);
+      setCategories(cat => [ 'Hola Mundo' ,...cat ]);
+  }
+
   return (
     <>
+
+      {/* titulo */}
+      
+      <button onClick={ onAddCategory }>Agregar</button>
+
       <h1>
         GifExpetApp
       </h1>
+
+      {/* input */}
+
+      <input />
+
+      {/* Lista */}
+      <ol>
+        { categories.map( category => {
+          return <li key={ category }>{ category }</li>
+        }) }
+      </ol>
+        {/* Gif items */}
+
     </>
   )
 };
